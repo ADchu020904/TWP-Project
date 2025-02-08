a38eb7fd6e1eb04596e8c36efe5a8eca4a6ebc44
gohjiahe <jiahewu1234567@gmail.com>
2025-02-08 21:51:35 +0800

    1.0

--- Template/connect.php
+++ Template/connect.php
@@ -9,4 +9,11 @@ $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect to the database".$conn->connect_error;
 }
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 ?>
