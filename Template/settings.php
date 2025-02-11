ace6392218be1dae91c4b5fba8988a795c83e078
jeremyhje <jeremyhje105@gmail.com>
2025-02-12 03:13:33 +0800

    enhance phone number input field with icon and improved styling

--- Template/settings.php
+++ Template/settings.php
@@ -106,7 +106,7 @@
                                   placeholder=""
                                   value="<?php echo htmlspecialchars($staff['name']); ?>"
                                 />
-                                <span class="absolute left-3 top-1/2 -translate-y-1/2 flex items-center">
+                                <span class="absolute left-3 top-1/2 -translate-y-1/2 flex items-center fill-current">
                                   <!-- SVG Icon -->
                                   <svg
                                     class="fill-current h-5 w-5"
@@ -142,14 +142,21 @@
                                 for="phoneNumber"
                                 >Phone Number</label
                               >
-                              <input
-                                class="w-full rounded-lg border border-stroke bg-gray py-2.5 px-4.5 font-medium text-black focus:border-primary focus-ring-2 focus:ring-primary/10 dark:border-strokedark dark:bg-boxdark dark:text-white"
-                                type="text"
-                                name="phone_number"
-                                id="phoneNumber"
-                                placeholder=""
-                                value="<?php echo htmlspecialchars($staff['phone_number']); ?>"
-                              />
+                              <div class="relative">
+                                <input
+                                  class="w-full rounded-lg border border-stroke bg-gray py-2.5 pl-9 pr-4.5 font-medium text-black focus:border-primary focus-ring-2 focus:ring-primary/10 dark:border-strokedark dark:bg-boxdark dark:text-white"
+                                  type="text"
+                                  name="phone_number"
+                                  id="phoneNumber"
+                                  placeholder=""
+                                  value="<?php echo htmlspecialchars($staff['phone_number']); ?>"
+                                />
+                                <span class="absolute left-3 top-1/2 -translate-y-1/2 flex items-center fill-current">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                                  </svg>
+                                </span>
+                              </div>
                             </div>
                           </div>
                         </div>
