f8f8127037f54e7ec6c935436222d39a8ab8ad49
jeremyhje <jeremyhje105@gmail.com>
2025-02-12 19:26:36 +0800

    feat: add .htaccess files for bootstrap initialization and create direct output test scripts

---
+++ Vik/direct.php
@@ -0,0 +1,7 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "<!-- Direct Test: __FILE__ = " . __FILE__ . " -->\n";
+echo "Direct output test in Vik folder is working!";
+?>
