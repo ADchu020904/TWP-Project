77f8861625ba2903b2b635055437d8c9b304736d
jeremyhje <jeremyhje105@gmail.com>
2025-02-12 16:54:04 +0800

    update .htaccess for relative paths; enable error display in init and session management

--- Vik/partials/session.php
+++ Vik/partials/session.php
@@ -4,6 +4,6 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 //Uncomment the following lines to display errors
-   //ini_set('display_errors', 1);
-        //error_reporting(E_ALL);
+   ini_set('display_errors', 1);
+        error_reporting(E_ALL);
 ?>
